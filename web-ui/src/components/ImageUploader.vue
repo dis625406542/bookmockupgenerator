a526fc2b13901d4a87db15428302ece2de24884b
dis625406542 <glm625406542@gmail.com>
2025-08-19 13:02:22 +0800

    生成器修改

--- web-ui/src/components/ImageUploader.vue
+++ web-ui/src/components/ImageUploader.vue
@@ -115,10 +115,10 @@ export default {
 
 <style scoped>
 .cropper-container {
-  width: 100%;
+  width: 80%;
   height: 350px;
   margin-bottom: 1rem;
-  background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAAA3NCSVQICAjb4U/gAAAABlBMVEXMzMz////TjRV2AAAACXBIWXMAAArrAAAK6wGCiw1aAAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M26LyyjAAAABFJREFUCJlj+M/AgBVhF/0PAH6/D/HkDxOGAAAAAElFTkSuQmCC');
+  background-image: url('data:image/png;base64,iVBORw0KGg');
 }
 
 .cropper-actions {
--- web-ui/src/views/Home.vue
+++ web-ui/src/views/Home.vue
@@ -55,7 +55,7 @@ export default {
   },
   data() {
     return {
-      coverImageUrl: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80',
+      coverImageUrl: require('@/assets/images/book-backImage.png'),
       showUploader: false,
       currentEditingType: 'cover',
     };
